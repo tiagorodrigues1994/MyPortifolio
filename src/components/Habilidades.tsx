3047205c7a9f4f2a0e0520eaf1826f3f425d3bde
Tiago Rodrigues <tiago.rodriguesfr2@gmail.com>
2024-05-16 10:02:47 -0300

    testando

--- src/components/Habilidades.tsx
+++ src/components/Habilidades.tsx
@@ -1,15 +1,7 @@
 
 import { Box, Grid, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'; 
-import { FaGithub } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
-import { SiTypescript } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { FaHtml5 } from "react-icons/fa";
-import { FaBootstrap } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { FaSass } from "react-icons/fa";
-import { SiStyledcomponents } from "react-icons/si";
+
 export default function Habilidades() {
   return (
     
